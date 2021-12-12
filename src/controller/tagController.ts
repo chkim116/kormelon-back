ae4b45ce54ee7f14447ba021d4c80150bfb06d00
김창회 <dudnqnfqlc@naver.com>
2021-12-13 05:51:55 +0900

    검색 api 수정

--- src/controller/tagController.ts
+++ src/controller/tagController.ts
@@ -1,70 +1,74 @@
-import Post from "../models/post"
-import { Request, Response } from "express"
+import Post from "../models/post";
+import { Request, Response } from "express";
 
 export const getTags = async (req: Request, res: Response) => {
     try {
         const tags = await Post.find({}, { tags: true }).sort({
             _id: -1,
-        })
-        let tagList: string[] = []
+        });
+        let tagList: string[] = [];
 
         tags.map((array: any) =>
             array.tags.map((tag: any) => tagList.push(tag))
-        )
+        );
 
         const reduceTag = tagList.reduce((obj: any, current) => {
             if (!obj[current]) {
-                obj[current] = 0
+                obj[current] = 0;
             }
-            obj[current]++
-            return obj
-        }, {})
+            obj[current]++;
+            return obj;
+        }, {});
 
         const tagsKeyValue = (Object.entries as any)(reduceTag).sort(
             (a: any, b: any) => b[1] - a[1]
-        )
+        );
 
-        res.status(200)
-        res.json(tagsKeyValue.slice(0, 20))
+        res.status(200);
+        res.json(tagsKeyValue.slice(0, 20));
     } catch (err) {
-        res.status(400)
-        res.json(err)
+        res.status(400);
+        res.json(err);
     }
-}
+};
 
 export const getSearchingTags = async (req: Request, res: Response) => {
-    const { select, tag, text } = req.query
+    const { select, tag, text, page } = req.query;
+    const pageNumber = page ? +page : 1;
 
     try {
-        let post
-        if (tag) {
-            post = await Post.find({
-                tags: tag as string,
-            }).sort({ _id: -1 })
-        }
+        let post;
+        let postCount = 0;
         if (select === "title") {
+            postCount = await Post.countDocuments({
+                title: text as string,
+            }).exec();
+
             post = await Post.find({
-                $or: [
-                    { title: { $regex: text as string } },
-                    { description: { $regex: text as string } },
-                ],
-            }).sort({
-                _id: -1,
+                $or: [{ title: { $regex: text as string } }],
             })
-        }
-        if (select === "description") {
-            post = await Post.find({
-                description: { $regex: text as string },
-            }).sort({
-                _id: -1,
-            })
+                .sort({
+                    _id: -1,
+                })
+                .limit(6)
+                .skip((pageNumber - 1) * 6)
+                .exec();
         }
         if (select === "tags") {
-            post = await Post.find({ tags: text as string }).sort({ _id: -1 })
+            postCount = await Post.countDocuments({
+                tags: text as string,
+            }).exec();
+
+            post = await Post.find({ tags: text as string })
+                .sort({ _id: -1 })
+                .limit(6)
+                .skip((pageNumber - 1) * 6)
+                .exec();
         }
-        res.status(200).json(post)
+
+        res.status(200).json({ post, postCount });
     } catch (err) {
-        res.status(400)
-        console.log(err)
+        res.status(400);
+        console.log(err);
     }
-}
+};
