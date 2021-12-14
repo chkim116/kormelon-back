4c793abd2fc3582132e3443f8359d0a324659516
김창회 <dudnqnfqlc@naver.com>
2021-12-14 23:35:03 +0900

    encodeURI -> encodeURIComponent

--- src/controller/postController.ts
+++ src/controller/postController.ts
@@ -1,44 +1,46 @@
-import { Request, Response } from "express"
-import Category from "../models/Category"
-import Post, { PostType } from "../models/post"
-import Comments from "../models/Comments"
-import { UserType } from "../models/User"
+import { Request, Response } from "express";
+import Category from "../models/Category";
+import Post, { PostType } from "../models/post";
+import Comments from "../models/Comments";
+import User, { UserType } from "../models/User";
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
 
 export const getAllTitle = async (req: Request, res: Response) => {
     try {
-        const post = await Post.find()
+        const post = await Post.find();
 
-        const postTitleList = post.map((lst) => lst.title)
-        res.json({ postTitleList })
+        const postTitleList = post.map((lst) => lst.title);
+        res.json({ postTitleList });
     } catch (error) {
-        console.error(error)
+        console.error(error);
     }
-}
+};
 
 export const getPost = async (req: Request, res: Response) => {
-    const { filter, page }: any = req.query
-    const pageNumber = parseInt(page || "1")
+    const { filter, page }: any = req.query;
+    const pageNumber = parseInt(page || "1");
 
     if (pageNumber < 1) {
-        res.status(400).send("페이지 에러")
-        return
+        res.status(400).send("페이지 에러");
+        return;
     }
     try {
         const post = await Post.find(
-            filter ? { category: decodeURI(filter) } : {}
+            filter ? { category: decodeURIComponent(filter) } : {}
         )
             .sort({ _id: -1 })
             .limit(6)
             .skip((pageNumber - 1) * 6)
-            .exec()
+            .exec();
         const postCount = await Post.countDocuments(
             filter ? { category: filter } : {}
-        ).exec()
-        res.json({ post, postCount })
+        ).exec();
+        res.json({ post, postCount });
     } catch (error) {
-        console.error(error)
+        console.error(error);
     }
-}
+};
 
 export const postPosting = async (req: Request, res: Response) => {
     const {
@@ -52,8 +54,8 @@ export const postPosting = async (req: Request, res: Response) => {
             tags,
             category,
         },
-    }: { body: PostType } = req
-    const id = (req.user as UserType)?._id as string
+    }: { body: PostType } = req;
+    const id = (req.user as UserType)?._id as string;
     try {
         const post = await Post.create({
             title,
@@ -65,54 +67,58 @@ export const postPosting = async (req: Request, res: Response) => {
             creator: id,
             tags: tags,
             category,
-        })
-        const categories = await Category.find({ category })
-        categories[0].post.push(post._id)
-        categories[0].save()
-        post.save()
-        res.json(true)
+        });
+        const categories = await Category.find({ category });
+        categories[0].post.push(post._id);
+        categories[0].save();
+        post.save();
+        res.json(true);
     } catch (err) {
-        console.error(err)
-        res.status(400)
-        res.json(false)
+        console.error(err);
+        res.status(400);
+        res.json(false);
     }
-}
+};
 
 export const getPostById = async (req: Request, res: Response) => {
-    const { title } = req.params
+    const { title } = req.params;
+    const token = req.cookies;
+    console.log(token, req.user);
 
     try {
-        const postByTitle = await Post.findOne({ title: decodeURI(title) })
+        const postByTitle = await Post.findOne({
+            title: decodeURIComponent(title),
+        });
         const nextPost = await Post.findOne({
             _id: { $gt: postByTitle?._id },
         })
             .sort({ _id: 1 })
-            .limit(1)
+            .limit(1);
         const prevPost = await Post.findOne({
             _id: { $lt: postByTitle?._id },
         })
             .sort({ _id: -1 })
-            .limit(1)
+            .limit(1);
 
         if (postByTitle) {
-            const view = postByTitle.views + 1
-            postByTitle.views = view
-            await postByTitle.save()
+            const view = postByTitle.views + 1;
+            postByTitle.views = view;
+            await postByTitle.save();
         }
         const next = nextPost
-            ? { id: nextPost?._id, title: encodeURI(nextPost.title) }
-            : null
+            ? { id: nextPost?._id, title: encodeURIComponent(nextPost.title) }
+            : null;
         const prev = prevPost
-            ? { id: prevPost?._id, title: encodeURI(prevPost?.title) }
-            : null
-        res.status(200).json({ postByTitle, next, prev })
+            ? { id: prevPost?._id, title: encodeURIComponent(prevPost?.title) }
+            : null;
+        res.status(200).json({ postByTitle, next, prev });
     } catch (err) {
-        console.error(err)
+        console.error(err);
     }
-}
+};
 
 export const postEditing = async (req: Request, res: Response) => {
-    const { id } = req.params
+    const { id } = req.params;
     const {
         title,
         preview,
@@ -121,24 +127,24 @@ export const postEditing = async (req: Request, res: Response) => {
         tags,
         category,
         thumb,
-    }: PostType = req.body
+    }: PostType = req.body;
     try {
         const post = await Post.findOneAndUpdate(
             { _id: id },
             { title, preview, description, updated, tags, category, thumb }
-        )
-        res.status(200).json(post)
+        );
+        res.status(200).json(post);
     } catch (err) {
-        res.status(400).send(false)
-        console.error(err)
+        res.status(400).send(false);
+        console.error(err);
     }
-}
+};
 
 export const postDeleting = async (req: Request, res: Response) => {
-    const { id, category } = req.params
+    const { id, category } = req.params;
     try {
-        await Post.findOneAndDelete({ _id: id })
-        const categories = await Category.find({ category })
+        await Post.findOneAndDelete({ _id: id });
+        const categories = await Category.find({ category });
         await Category.findOneAndUpdate(
             {
                 category,
@@ -148,57 +154,57 @@ export const postDeleting = async (req: Request, res: Response) => {
                     (list) => list.toString() !== id
                 ),
             }
-        )
+        );
 
-        res.status(200).send(true)
+        res.status(200).send(true);
     } catch (err) {
-        res.status(400).send(false)
-        console.error(err)
+        res.status(400).send(false);
+        console.error(err);
     }
-}
+};
 
 export const postComments = async (req: Request, res: Response) => {
-    const { id } = req.params
+    const { id } = req.params;
     const {
         comment: { comment },
         createDate,
-    } = req.body
+    } = req.body;
     try {
-        const post = await Post.findById(id)
+        const post = await Post.findById(id);
         const comments = await Comments.create({
             comment: comment as string,
             creator: req.user ? (req.user as UserType).username : "익명",
             createDate: createDate,
-        })
+        });
         if (post && comments) {
-            post.comment?.push(comments)
-            post.save()
-            res.status(200).json(comments)
+            post.comment?.push(comments);
+            post.save();
+            res.status(200).json(comments);
         }
     } catch (err) {
-        res.status(400)
-        console.error(err)
+        res.status(400);
+        console.error(err);
     }
-}
+};
 
 export const delComments = async (req: Request, res: Response) => {
-    const { id } = req.params
+    const { id } = req.params;
     try {
-        await Comments.findOneAndDelete({ _id: id })
-        res.status(200)
+        await Comments.findOneAndDelete({ _id: id });
+        res.status(200);
     } catch (err) {
-        res.status(400)
-        console.error(err)
+        res.status(400);
+        console.error(err);
     }
-}
+};
 
 export const postImg = (req: Request, res: Response) => {
-    const { file } = req
-    const location = (file as any).location
+    const { file } = req;
+    const location = (file as any).location;
     try {
-        res.status(200).json(location)
+        res.status(200).json(location);
     } catch (err) {
-        console.error(err)
-        res.status(400).json(err)
+        console.error(err);
+        res.status(400).json(err);
     }
-}
+};
