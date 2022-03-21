99f36e911a028131e6790af5f1b2877b0360f1b9
김창회 <michaelkim@dreamyoungs.com>
2022-03-21 12:40:57 +0900

    view -> views

--- src/model/repository/ViewRepository.ts
+++ src/model/repository/ViewRepository.ts
@@ -1,12 +1,12 @@
 import { EntityRepository, getCustomRepository, Repository } from 'typeorm';
-import { View } from '../entities/view';
+import { Views } from '../entities/Views';
 
 export function viewRepository() {
 	return getCustomRepository(ViewRepository, process.env.NODE_ENV);
 }
 
-@EntityRepository(View)
-export class ViewRepository extends Repository<View> {
+@EntityRepository(Views)
+export class ViewRepository extends Repository<Views> {
 	async getView() {
 		const view = await this.findOne({ where: { id: 1 } });
 
