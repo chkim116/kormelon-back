3f003d81e61d91521daf3bb03a61bcde51e0c8fe
kim changhoe <dudnqnfqlc@naver.com>
2022-03-28 16:19:30 +0900

    retry

--- scripts/eb-zip.sh
+++ scripts/eb-zip.sh
@@ -3,11 +3,11 @@
 stat dev || mkdir dev
 
 # Archive artifacts
-zip dev/kormelon-api.zip -r ./ -x "*.git/*" "./@types/*" "./.circleci/*" "./coverage/*" "./scripts/*" "./src/*" "./tsconfig.json" "./nodemon.json" "./jest.config.ts"
+zip dev/kormelon-api-v2.zip -r ./ -x "*.git/*" "./@types/*" "./.circleci/*" "./coverage/*" "./scripts/*" "./src/*" "./tsconfig.json" "./nodemon.json" "./jest.config.ts"
 
 # Setup eb config
 mkdir .elasticbeanstalk
 cat << EOF > .elasticbeanstalk/config.yml
 deploy:
-    artifact: dev/kormelon-api.zip
+    artifact: dev/kormelon-api-v2.zip
 EOF
